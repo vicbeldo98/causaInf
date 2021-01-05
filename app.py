2ad52207feb7558f007480ed2ea547623fa929d9
victoria <beltran@unitedpartners.de>
2021-01-05 18:31:23 +0100

    created session and added upload csv

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+from flask_session import Session
+
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config.from_object(__name__)
+app.secret_key = 'esto-es-una-clave-muy-secreta'
+Session(app)
 
 
 @app.route("/")
@@ -11,18 +17,16 @@ def main_page():
 
 @app.route('/compute-causal-effect', methods=['POST'])
 def compute():
-    print(request.form['graph'])
+    session['graph'] = request.form['graph']
+
     print('In ComputeCausalEffect')
+    print(session)
     return '', 200
 
 
 @app.route('/upload-csv', methods=['POST'])
 def uploadCSV():
-    print(request.files)
-    if 'file' not in request.files:
-        print('No file part')
-    else:
-        print(request.files['file'])
+    session['csv_content'] = request.form['file-content']
     print('In UploadCSV')
     return '', 200
 
