e32992868d10e9574692611f4f3c0392d97e5aa2
victoria <beltran@unitedpartners.de>
2021-02-06 19:17:32 +0100

    handling of errors

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, json
 from flask_session import Session
 from causal_effect import estimate_effect_with_estimand, estimate_with_variables, compute_estimands
-import json
 
 
 app = Flask(__name__)
@@ -18,14 +17,29 @@ def main_page():
 
 @app.route('/compute-effect-from-graph', methods=['POST'])
 def compute_effect_from_graph():
+    if 'csv_content' not in session.keys():
+        raise Exception('No csv file found. Please, upload one first.')
+    if request.form['treatment'] == '':
+        raise Exception('Create exposure variables on the graph')
+    if request.form['outcome'] == '':
+        raise Exception('Create outcome variable on the graph')
+
     causal_effect = estimate_with_variables(session['csv_content'], request.form['graph'], request.form['treatment'], request.form['outcome'], request.form['adjusted'])
     return {'treatment' : request.form['treatment'], 'outcome' : request.form['outcome'], 'effect' : causal_effect}
 
 
 @app.route('/retrieve-estimands', methods=['POST'])
 def retrieve_estimands():
-    session['treatment'] = request.form['treatment']
-    session['outcome'] = request.form['outcome']
+    if 'csv_content' not in session.keys():
+        raise Exception('No csv file found. Please, upload one first.')
+    if request.form['treatment'] == '':
+        raise Exception('Create exposure variables on the graph')
+    else:
+        session['treatment'] = request.form['treatment']
+    if request.form['outcome'] == '':
+        raise Exception('Create outcome variable on the graph')
+    else:
+        session['outcome'] = request.form['outcome']
     causal_estimands, session['model'], session['identified_estimand'] = compute_estimands(session['csv_content'], request.form['graph'], request.form['treatment'], request.form['outcome'])
     return json.dumps(dict(causal_estimands), default=str)
 
@@ -39,8 +53,12 @@ def compute_effect_with_estimand():
 @app.route('/upload-csv', methods=['POST'])
 def upload_csv():
     session['csv_content'] = request.form['file-content']
-    print('In UploadCSV')
     return '', 200
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return str(e), 500
 
 
 app.run(debug=False)
--- causal_effect.py
+++ causal_effect.py
@@ -6,7 +6,12 @@ from sklearn.linear_model import LassoCV
 from sklearn.ensemble import GradientBoostingRegressor
 import json
 
-CANNOT_FIND_SUITABLE_ESTIMAND = 'Sorry, we can not find a suitable estimand for the selected variables. To check the available ones, click on "Compute manually"'
+CANNOT_FIND_SUITABLE_ESTIMAND = 'Sorry, we can not find a suitable estimand for the adjusted graph variables. To check for the available ones, click on "Compute with options".'
+EMPTY_CSV = 'No data found on the uploaded csv. Please, select another.'
+PARSE_ERROR_CSV = 'Error parsing the csv. Please, select another.'
+CSV_ERROR = 'Something went wrong with the csv loading. Please, select another.'
+PROBLEM_CAUSAL_MODEL_GRAPH = 'Something went wrong when estimating the Causal Model. Please, make sure that all nodes in the graph have a corresponding csv column, that the graph is directed and acyclic and that treatment and outcome variables are correctly marked on the graph.'
+PROBLEM_CAUSAL_MODEL_MANUAL = 'Something went wrong when estimating the Causal Model.'
 
 
 def load_csv(csv_content):
@@ -14,36 +19,37 @@ def load_csv(csv_content):
         data = StringIO(csv_content)
         dataframe = pd.read_csv(data, sep=",", engine='c', skipinitialspace=True)
         return dataframe
-    except BaseException as e:
-        raise Exception('{}'.format(e))
+    except pd.errors.EmptyDataError:
+        raise Exception('{}'.format(EMPTY_CSV))
+    except pd.errors.ParserError:
+        raise Exception('{}'.format(PARSE_ERROR_CSV))
+    except Exception:
+        raise Exception('{}'.format(CSV_ERROR))
 
 
 def compute_estimands(csv_content, graph, treatment, outcome):
-    try:
-        data = load_csv(csv_content)
-        model = CausalModel(
-            data=data,
-            treatment=treatment.split(','),
-            outcome=outcome.split(','),
-            graph=graph,
-            proceed_when_unidentifiable=True
-        )
-        identified_estimand = model.identify_effect()
-        result = dict(identified_estimand.estimands.items())
-        backdoor_dict = identified_estimand.backdoor_variables
-        frontdoor_dict = identified_estimand.get_frontdoor_variables()
-        iv_estimands = identified_estimand.get_instrumental_variables()
-        if result['frontdoor'] is None:
-            del result['frontdoor']
-        else:
-            result['frontdoor']['related_variables'] = frontdoor_dict
-        if iv_estimands != []:
-            result['iv']['related_variables'] = iv_estimands
-        for b in backdoor_dict.keys():
-            result[b]['related_variables'] = backdoor_dict[b]
-        return result, model, identified_estimand
-    except BaseException as e:
-        raise Exception('{}'.format(e))
+    data = load_csv(csv_content)
+    model = CausalModel(
+        data=data,
+        treatment=treatment.split(','),
+        outcome=outcome.split(','),
+        graph=graph,
+        proceed_when_unidentifiable=True
+    )
+    identified_estimand = model.identify_effect()
+    result = dict(identified_estimand.estimands.items())
+    backdoor_dict = identified_estimand.backdoor_variables
+    frontdoor_dict = identified_estimand.get_frontdoor_variables()
+    iv_estimands = identified_estimand.get_instrumental_variables()
+    if result['frontdoor'] is None:
+        del result['frontdoor']
+    else:
+        result['frontdoor']['related_variables'] = frontdoor_dict
+    if iv_estimands != []:
+        result['iv']['related_variables'] = iv_estimands
+    for b in backdoor_dict.keys():
+        result[b]['related_variables'] = backdoor_dict[b]
+    return result, model, identified_estimand
 
 
 def retrieveCorrespondingEstimand(identified_estimand, treatment, outcome, adjusted):
@@ -73,27 +79,32 @@ def estimate_with_variables(csv_content, graph, treatment, outcome, adjusted):
         outcome=outcome.split(','),
         graph=graph,
         proceed_when_unidentifiable=True)
-
     identified_estimand = model.identify_effect()
     estimand_name = retrieveCorrespondingEstimand(identified_estimand, treatment, outcome, adjusted)
     if estimand_name is None:
         raise Exception(CANNOT_FIND_SUITABLE_ESTIMAND)
-    causal_effect = estimate_effect_with_estimand(model, identified_estimand, estimand_name)
-
+    causal_effect = estimate_effect_with_estimand(model, identified_estimand, estimand_name, True)
     return causal_effect
 
 
-def estimate_effect_with_estimand(model, identified_estimand, estimand_name):
-    dml_estimate = model.estimate_effect(identified_estimand,
-                                         method_name=estimand_name + ".econml.dml.DML",
-                                         control_value=0,
-                                         treatment_value=1,
-                                         target_units="ate",
-                                         confidence_intervals=False,
-                                         method_params={"init_params": {'model_y': GradientBoostingRegressor(),
-                                                                        'model_t': GradientBoostingRegressor(),
-                                                                        "model_final": LassoCV(fit_intercept=False),
-                                                                        'featurizer': PolynomialFeatures(degree=1, include_bias=False)},
-                                                        "fit_params": {}}
-                                         )
-    return round(dml_estimate.value, 3)
+def estimate_effect_with_estimand(model, identified_estimand, estimand_name, from_graph=False):
+    try:
+        dml_estimate = model.estimate_effect(identified_estimand,
+                                             method_name=estimand_name + ".econml.dml.DML",
+                                             control_value=0,
+                                             treatment_value=1,
+                                             target_units="ate",
+                                             confidence_intervals=False,
+                                             method_params={"init_params": {'model_y': GradientBoostingRegressor(),
+                                                                            'model_t': GradientBoostingRegressor(),
+                                                                            "model_final": LassoCV(fit_intercept=False),
+                                                                            'featurizer': PolynomialFeatures(degree=1, include_bias=False)},
+                                                            "fit_params": {}}
+                                             )
+        return round(dml_estimate.value, 3)
+    except Exception:
+        if from_graph:
+            exception_msg = PROBLEM_CAUSAL_MODEL_GRAPH
+        else:
+            exception_msg = PROBLEM_CAUSAL_MODEL_MANUAL
+        raise Exception('{}'.format(exception_msg))
